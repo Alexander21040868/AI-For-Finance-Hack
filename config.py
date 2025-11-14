633a2c7f11a83944f1bc38139fb4172557d85591
Matvey Perminov <mectu@mail.ru>
2025-11-14 18:40:58 +0300

    Improved structure

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения (API ключи) из файла .env
+load_dotenv()
+
+# Ключ для доступа к API
+OPEN_ROUTER_API_KEY = os.getenv("OPEN_ROUTER_API_KEY")
+
+# Базовый URL для всех запросов
+BASE_URL = "https://openrouter.ai/api/v1"
+
+LOGGING_TOKEN_USAGE = True  # Логгировать использование токенов
+LOGGING_TIME_USAGE = True  # Логгировать использование времени
+
+# Файлы для артефактов
+REGULATORY_CONSULTANT_FAISS_INDEX_PATH = "artefacts/regulatory_consultant_faiss_index.bin"
+REGULATORY_CONSULTANT_CHUNKS_PATH = "artefacts/corpus_chunks.pkl"
+
+# Использование локальных файлов RAG
+USE_LOCAL_RAG_FILES = True
+SAVE_RAG_FILES = True
